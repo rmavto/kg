7e203972c8bb0285cdfb54a06067790c6b473a2f
narynbek <asankulovnarynbek2006@gmail.com>
2022-12-01 21:21:43 +0600

    feat: modal

---
+++ script.js
@@ -0,0 +1,27 @@
+var mainListDiv = document.getElementById("mainListDiv"),
+  mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+  "use strict";
+
+  mainListDiv.classList.toggle("show_list");
+  mediaButton.classList.toggle("active");
+};
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".float");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
